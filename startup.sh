5abd1c2dacc7c998246cc0cd7339e0f6f303c1fb
Mateusz Brycki <mateusz.brycki@outlook.com>
2018-09-11 22:20:28 +0200

    main - configure Eureka Server and Clients, startup scripts,

--- startup.sh
+++ startup.sh
@@ -1,8 +1,26 @@
 echo "------------------------ CLOSING OLD SESSIONS -------------------------------"
 screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
 
+echo "------------------------ IS ZOOKEEPER RUNNING? ------------------------------"
+lsof -nP -i4TCP:2181 | grep LISTEN;
+
+echo "------------------------ IS KAFKA RUNNING? ----------------------------------"
+lsof -nP -i4TCP:9092 | grep LISTEN;
+
+
 echo "------------------------ STARTING ZOOKEEPER SERVER --------------------------"
-screen -d -m -S "zookeeper-server" bash -c "kafka/bin/zookeeper-server-start.sh config/zookeeper.properties";
+screen -d -m -S "zookeeper-server" bash -c "kafka/bin/zookeeper-server-start.sh kafka/config/zookeeper.properties";
 
 echo "------------------------ STARTING KAFKA SERVER ------------------------------"
-screen -d -m -S "kafka-server" bash -c "kafka/bin/kafka-server-start.sh config/server.properties";
+screen -d -m -S "kafka-server" bash -c "kafka/bin/kafka-server-start.sh kafka/config/server.properties";
+
+echo "------------------------ STARTING EUREKA SERVER -----------------------------"
+screen -d -m -S "eureka-server" bash -c "cd eureka-server; ./startup.sh";
+
+
+echo "------------------------ STARTING EXAMPLE1 ----------------------------------"
+screen -d -m -S "example1" bash -c "cd example1; ./startup.sh";
+
+
+echo "------------------------ STARTING EXAMPLE2 ----------------------------------"
+screen -d -m -S "example2" bash -c "cd example2; ./startup.sh";
