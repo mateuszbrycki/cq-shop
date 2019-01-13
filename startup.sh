50ad698045ab4cf382e4269a6627a341162a5389
Mateusz Brycki <mateusz.brycki@outlook.com>
2019-01-13 09:46:49 +0100

    OTHER-3 - Move all projects to Java 11

--- startup.sh
+++ startup.sh
@@ -22,10 +22,23 @@ screen -d -m -S "config-server" bash -c "cd config-server; ./startup.sh";
 echo "------------------------ STARTING EUREKA SERVER -----------------------------"
 screen -d -m -S "eureka-server" bash -c "cd eureka-server; ./startup.sh";
 
-#echo "------------------------ STARTING USER MANAGEMENT SERVICE  ------------------"
-#screen -d -m -S "user-management-service" bash -c "cd user-management-service; ./startup.sh";
+#echo "------------------------ STARTING API GATEWAY SERVICE  ---------------------"
+#screen -d -m -S "api-gateway-service" bash -c "cd api-gateway-service; ./startup.sh";
+
+#echo "------------------------ STARTING CART SERVICE  -----------------------------"
+#screen -d -m -S "cart-service" bash -c "cd cart-service; ./startup.sh";
 
 #echo "------------------------ STARTING LOGGING SERVICE  --------------------------"
 #screen -d -m -S "logging-service" bash -c "cd logging-service; ./startup.sh";
 
-#bin/schema-registry-start ./etc/schema-registry/schema-registry.properties
+#echo "------------------------ STARTING NOTIFICATION SERVICE  ---------------------"
+#screen -d -m -S "notification-service" bash -c "cd notification-service; ./startup.sh";
+
+#echo "------------------------ STARTING ORDER SERVICE  ----------------------------"
+#screen -d -m -S "order-service" bash -c "cd order-service; ./startup.sh";
+
+#echo "------------------------ STARTING USER MANAGEMENT SERVICE  ------------------"
+#screen -d -m -S "user-management-service" bash -c "cd user-management-service; ./startup.sh";
+
+#echo "------------------------ STARTING WAREHOUSE SERVICE  ------------------------"
+#screen -d -m -S "warehouse-service" bash -c "cd warehouse-service; ./startup.sh";
