f81ffa8f87ab99844acb12f44cef46449eebc6c1
Mateusz Brycki <mateusz.brycki@outlook.com>
2019-02-16 15:03:18 +0100

    PRODUCT-4 Add endpoint for listing all products

---
+++ warehouse-service/src/main/java/com/cqshop/warehouse/dto/Product.java
@@ -0,0 +1,33 @@
+package com.cqshop.warehouse.dto;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+/**
+ * Created by Mateusz Brycki on 26/12/2018.
+ */
+@AllArgsConstructor
+@Builder
+@Getter
+@Entity
+@EqualsAndHashCode
+public class Product {
+
+    private Long productId;
+    private String name;
+    private String code;
+    private String description;
+    private Integer quantity;
+
+    public static Product of(com.cqshop.warehouse.domain.Product product) {
+
+        return Product.builder()
+                .productId(product.getProductId())
+                .name(product.getName())
+                .code(product.getCode())
+                .description(product.getDescription())
+                .quantity(product.getQuantity())
+                .build();
+    }
+}
